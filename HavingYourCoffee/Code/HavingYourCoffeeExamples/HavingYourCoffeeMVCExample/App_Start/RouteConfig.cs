8b3f3902577aaa6af56b1def66f98627bdf0c68e
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2013-05-30 11:56:22 -0400

    Cleaned up the project

--- HavingYourCoffee/Code/HavingYourCoffeeExamples/HavingYourCoffeeMVCExample/App_Start/RouteConfig.cs
+++ HavingYourCoffee/Code/HavingYourCoffeeExamples/HavingYourCoffeeMVCExample/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HavingYourCoffeeMVCExample
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "RomanNumeral", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- HavingYourCoffee/Code/HavingYourCoffeeExamples/HavingYourCoffeeMVCExample/Filters/InitializeSimpleMembershipAttribute.cs
+++ HavingYourCoffee/Code/HavingYourCoffeeExamples/HavingYourCoffeeMVCExample/Filters/InitializeSimpleMembershipAttribute.cs
@@ -4,7 +4,6 @@ using System.Data.Entity.Infrastructure;
 using System.Threading;
 using System.Web.Mvc;
 using WebMatrix.WebData;
-using HavingYourCoffeeMVCExample.Models;
 
 namespace HavingYourCoffeeMVCExample.Filters
 {
@@ -25,25 +24,7 @@ namespace HavingYourCoffeeMVCExample.Filters
         {
             public SimpleMembershipInitializer()
             {
-                Database.SetInitializer<UsersContext>(null);
-
-                try
-                {
-                    using (var context = new UsersContext())
-                    {
-                        if (!context.Database.Exists())
-                        {
-                            // Create the SimpleMembership database without Entity Framework migration schema
-                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
-                        }
-                    }
-
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
-                }
+                
             }
         }
     }
